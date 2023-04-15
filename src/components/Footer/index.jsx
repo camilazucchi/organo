da84f279522f83dac3e2e060642b4a26a01aabe8
Camila Zucchi <camilazucchi@outlook.com>
2023-04-15 18:32:19 -0300

    style: add responsive design

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,6 +3,8 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
+      <img className="logo" src="/images/logo-tft.png" alt="Logo do Organo"></img>
+      <p>Desenvolvido por Camila Zucchi.</p>
       <div className="footer-icons">
         <a href="facebook.com" target="_blank">
           <img className="icons" src="/images/fb.png" alt="Logo do Facebook" />
@@ -14,8 +16,6 @@ const Footer = () => {
           <img className="icons" src="/images/ig.png" alt="Logo do Instagram" />
         </a>
       </div>
-      <img className="logo" src="/images/logo-tft.png" alt="Logo do Organo"></img>
-      <p>Desenvolvido por Camila Zucchi.</p>
     </footer>
   );
 };
